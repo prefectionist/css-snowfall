79d6f22f3435469cae824dbb3c0bd1d7c2e11307
prefect <prefect@10mm.com>
2020-01-07 10:22:49 -0500

    adding JS to generate `flakes`

--- src/js/main/script.js
+++ src/js/main/script.js
@@ -3,7 +3,8 @@ let flakeCount = 10;
 
 let newFlake = null;
 for (var i=0; i<flakeCount; i++) {
-  newFlake = document.createElement('div');
-  newFlake.className += 'flake-'+i;
-  console.log(newFlake);
+  this.newFlake = document.createElement('div');
+  this.newFlake.className += 'flake-'+i;
+  console.log(this.newFlake);
+  snowfall.append(this.newFlake);
 }
